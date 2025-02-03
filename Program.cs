6d9602f9b3ba28a25cef1e2acfc65d3b0c5319b0
MatthewJasengnou <132300434+MatthewJasengnou@users.noreply.github.com>
2025-02-03 10:01:44 -0500

    Updated configuration for minimal API

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using Microsoft.AspNetCore.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
