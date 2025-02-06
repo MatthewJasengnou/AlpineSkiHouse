5663ebd7435d0e729608bc087042f64487cc9a33
MatthewJasengnou <132300434+MatthewJasengnou@users.noreply.github.com>
2025-02-06 10:41:58 -0500

    Updated

--- models/Activity.cs
+++ models/Activity.cs
@@ -6,8 +6,8 @@ namespace AlpineSkiHouse.Models
     public class Activity
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; } //Needs to be nullable "?"
+        public string? Description { get; set; } //Needs to be nullable "?"
         public int Likes { get; set; }
         public int Dislikes { get; set; }
 
